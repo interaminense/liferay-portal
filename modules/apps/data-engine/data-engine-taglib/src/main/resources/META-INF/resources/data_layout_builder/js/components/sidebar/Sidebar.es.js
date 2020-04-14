ff668f4bd4b137cd8e718b497adb987b9cf4e04a
Keven <keven.leone@liferay.com>
2020-04-14 12:31:32 -0300

    LPS-111624 add margin on FormView sidebar

--- modules/apps/data-engine/data-engine-taglib/src/main/resources/META-INF/resources/data_layout_builder/js/components/sidebar/Sidebar.es.js
+++ modules/apps/data-engine/data-engine-taglib/src/main/resources/META-INF/resources/data_layout_builder/js/components/sidebar/Sidebar.es.js
@@ -22,7 +22,7 @@ const Sidebar = React.forwardRef(
 	({children, className, onSearch = null}, ref) => {
 		return (
 			<div
-				className={classNames(className, 'data-layout-builder-sidebar')}
+				className={classNames(className, 'data-layout-builder-sidebar mr-4')}
 				ref={ref}
 			>
 				<div className="sidebar">
