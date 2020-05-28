0e3f42506a80c29af56115819b6c1e0a89470627
Adriano Interaminense <adriano.interaminense@gmail.com>
2020-05-28 10:37:26 -0300

    LPS-111991 add FieldSet modal and language properties

--- modules/apps/data-engine/data-engine-taglib/src/main/resources/META-INF/resources/data_layout_builder/js/App.es.js
+++ modules/apps/data-engine/data-engine-taglib/src/main/resources/META-INF/resources/data_layout_builder/js/App.es.js
@@ -40,7 +40,9 @@ const parseProps = ({
 });
 
 const AppContent = ({
+	caller,
 	dataLayoutBuilder,
+	setChildrenContext,
 	setDataLayoutBuilder,
 	sidebarConfig,
 	...props
@@ -53,7 +55,10 @@ const AppContent = ({
 		if (dataLayoutBuilder) {
 			dataLayoutBuilder.emit('contextUpdated', state);
 		}
-	}, [dataLayoutBuilder, state]);
+		if (caller) {
+			setChildrenContext({dispatch, state});
+		}
+	}, [caller, dataLayoutBuilder, dispatch, setChildrenContext, state]);
 
 	return (
 		<>
@@ -85,6 +90,7 @@ const AppContent = ({
 
 const App = (props) => {
 	const {
+		caller = 'App',
 		config,
 		contentType,
 		dataDefinitionId,
@@ -94,8 +100,8 @@ const App = (props) => {
 		groupId,
 	} = parseProps(props);
 
+	window[caller] = props;
 	const sidebarConfig = initializeSidebarConfig(props);
-
 	const [loaded, setLoaded] = useState(false);
 	const [dataLayoutBuilder, setDataLayoutBuilder] = useState(null);
 
