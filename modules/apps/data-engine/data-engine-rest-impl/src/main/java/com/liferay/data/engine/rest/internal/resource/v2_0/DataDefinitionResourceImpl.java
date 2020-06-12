69eee63904f3296bab75a4b33a0e60693fbcebb9
Jeyvison Nascimento <jeyvison.nascimento@liferay.com>
2020-06-12 15:35:07 -0300

    LPS-113702 Add more SQLTypes to date field

--- modules/apps/data-engine/data-engine-rest-impl/src/main/java/com/liferay/data/engine/rest/internal/resource/v2_0/DataDefinitionResourceImpl.java
+++ modules/apps/data-engine/data-engine-rest-impl/src/main/java/com/liferay/data/engine/rest/internal/resource/v2_0/DataDefinitionResourceImpl.java
@@ -843,7 +843,9 @@ public class DataDefinitionResourceImpl
 
 			type = "numeric";
 		}
-		else if (sqlType == Types.DATE) {
+		else if ((sqlType == Types.DATE) || (sqlType == Types.TIME) ||
+				 (sqlType == Types.TIMESTAMP)) {
+
 			type = "date";
 		}
 
