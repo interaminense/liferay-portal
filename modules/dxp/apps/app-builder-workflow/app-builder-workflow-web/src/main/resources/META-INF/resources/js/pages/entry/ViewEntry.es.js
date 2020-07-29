016d037458fe2bf8c9aa2ee3b5635ea3dc93608a
Albino Junior <albino.junior@liferay.com>
2020-07-29 14:48:47 -0300

    LPS-117970 Normalize data layouts names on ViewEntry

--- modules/dxp/apps/app-builder-workflow/app-builder-workflow-web/src/main/resources/META-INF/resources/js/pages/entry/ViewEntry.es.js
+++ modules/dxp/apps/app-builder-workflow/app-builder-workflow-web/src/main/resources/META-INF/resources/js/pages/entry/ViewEntry.es.js
@@ -16,11 +16,9 @@ import useQuery from 'app-builder-web/js/hooks/useQuery.es';
 import {ViewDataLayoutPageValues} from 'app-builder-web/js/pages/entry/ViewEntry.es';
 import ViewEntryUpperToolbar from 'app-builder-web/js/pages/entry/ViewEntryUpperToolbar.es';
 import {getItem} from 'app-builder-web/js/utils/client.es';
+import {getLocalizedValue} from 'app-builder-web/js/utils/lang.es';
 import {errorToast} from 'app-builder-web/js/utils/toast.es';
-import {
-	getTranslatedValue,
-	isEqualObjects,
-} from 'app-builder-web/js/utils/utils.es';
+import {isEqualObjects} from 'app-builder-web/js/utils/utils.es';
 import {usePrevious} from 'frontend-js-react-web';
 import React, {useContext, useEffect, useMemo, useState} from 'react';
 
@@ -178,9 +176,9 @@ export default function ViewEntry({
 									({dataLayoutPages = [], ...dataLayout}) => (
 										<>
 											<h3>
-												{getTranslatedValue(
-													dataLayout,
-													'name'
+												{getLocalizedValue(
+													dataDefinition.defaultLanguageId,
+													dataLayout.name
 												)}
 											</h3>
 
