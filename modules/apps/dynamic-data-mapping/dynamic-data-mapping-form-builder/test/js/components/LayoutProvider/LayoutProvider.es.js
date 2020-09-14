4749dc97c2cc7a9830bbd68c373fbb8b416d20ca
Adriano Interaminense <adriano.interaminense@gmail.com>
2020-09-14 09:11:35 -0300

    LPS-120441 Auto SF

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-builder/test/js/components/LayoutProvider/LayoutProvider.es.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-builder/test/js/components/LayoutProvider/LayoutProvider.es.js
@@ -567,8 +567,8 @@ describe('LayoutProvider', () => {
 									// toMatchSnapshot has problems with deep arrays so we override it here to
 									// avoid this.
 
-									instanceId: 'Any<String>'
-								}))
+									instanceId: 'Any<String>',
+								}));
 							}
 
 							return {
@@ -583,8 +583,8 @@ describe('LayoutProvider', () => {
 								name: `name${fieldIndex}${columnIndex}${rowIndex}${pageIndex}`,
 								settingsContext: {
 									...field.settingsContext,
-									pages: newPages
-								}
+									pages: newPages,
+								},
 							};
 						}
 					)
