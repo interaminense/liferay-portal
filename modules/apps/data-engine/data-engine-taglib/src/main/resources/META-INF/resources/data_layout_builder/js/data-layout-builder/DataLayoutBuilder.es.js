f1ebc4f7f9d2c5bc9819868a2ffa15ad871ae149
Adriano Interaminense <adriano.interaminense@gmail.com>
2021-02-25 02:52:30 -0300

    LPS-123802 Do not normalize labelAtStructureLevel before save form

--- modules/apps/data-engine/data-engine-taglib/src/main/resources/META-INF/resources/data_layout_builder/js/data-layout-builder/DataLayoutBuilder.es.js
+++ modules/apps/data-engine/data-engine-taglib/src/main/resources/META-INF/resources/data_layout_builder/js/data-layout-builder/DataLayoutBuilder.es.js
@@ -402,7 +402,6 @@ class DataLayoutBuilder extends React.Component {
 				editingLanguageId,
 			]),
 		];
-		const focusedField = layoutProvider.getFocusedField();
 
 		this.setState({
 			availableLanguageIds,
@@ -425,6 +424,8 @@ class DataLayoutBuilder extends React.Component {
 		};
 
 		this.formBuilderWithLayoutProvider.props.layoutProviderProps = this.formBuilderWithLayoutProvider.props.layoutProviderProps; // eslint-disable-line
+
+		const focusedField = layoutProvider.getFocusedField();
 
 		if (Object.keys(focusedField).length) {
 			layoutProvider
