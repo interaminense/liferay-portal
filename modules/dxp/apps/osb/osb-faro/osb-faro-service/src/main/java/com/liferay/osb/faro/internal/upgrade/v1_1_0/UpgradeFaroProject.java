33f2f5280c3205fb2c7db519a32b23097bfe9232
leilanyaragao <leilanyaragao26@gmail.com>
2023-02-24 17:53:18 -0300

    LRAC-11500 add osb-faro-service

---
+++ modules/dxp/apps/osb/osb-faro/osb-faro-service/src/main/java/com/liferay/osb/faro/internal/upgrade/v1_1_0/UpgradeFaroProject.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.osb.faro.internal.upgrade.v1_1_0;
+
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+
+/**
+ * @author Matthew Kong
+ */
+public class UpgradeFaroProject extends UpgradeProcess {
+
+	@Override
+	protected void doUpgrade() throws Exception {
+		alterTableAddColumn("OSBFaro_FaroProject", "accountKey", "STRING");
+		alterTableAddColumn("OSBFaro_FaroProject", "accountName", "STRING");
+		alterTableAddColumn("OSBFaro_FaroProject", "code_", "STRING");
+		alterTableAddColumn("OSBFaro_FaroProject", "subscription", "STRING");
+	}
+
+}
