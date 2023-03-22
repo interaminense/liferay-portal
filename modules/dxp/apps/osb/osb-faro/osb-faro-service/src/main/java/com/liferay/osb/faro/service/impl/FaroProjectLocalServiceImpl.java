brandonyee333/test

LRAC-11500 Migrate osb-faro-service
Co-authored-by: Adriano Interaminense <adriano.interaminense@gmail.com>

modules/dxp/apps/osb/osb-faro/osb-faro-service/src/main/java/com/liferay/osb/faro/service/impl/FaroProjectLocalServiceImpl.java
@@ -20,8 +20,13 @@ import com.liferay.osb.faro.constants.DocumentationConstants;
 import com.liferay.osb.faro.constants.FaroProjectConstants;
 import com.liferay.osb.faro.model.FaroProject;
 import com.liferay.osb.faro.model.FaroUser;
+import com.liferay.osb.faro.service.FaroChannelLocalService;
+import com.liferay.osb.faro.service.FaroPreferencesLocalService;
+import com.liferay.osb.faro.service.FaroProjectEmailDomainLocalService;
+import com.liferay.osb.faro.service.FaroUserLocalService;
 import com.liferay.osb.faro.service.base.FaroProjectLocalServiceBaseImpl;
 import com.liferay.osb.faro.util.EmailUtil;
+import com.liferay.petra.sql.dsl.query.DSLQuery;
 import com.liferay.petra.string.CharPool;
 import com.liferay.petra.string.StringBundler;
 import com.liferay.portal.kernel.dao.orm.QueryUtil;
@@ -34,25 +39,31 @@ import com.liferay.portal.kernel.model.GroupConstants;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.search.Indexable;
 import com.liferay.portal.kernel.search.IndexableType;
+import com.liferay.portal.kernel.service.GroupLocalService;
+import com.liferay.portal.kernel.service.UserLocalService;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.ResourceBundleUtil;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.util.comparator.GroupNameComparator;
-import com.liferay.portal.spring.extender.service.ServiceReference;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.ResourceBundle;
-import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import javax.mail.internet.InternetAddress;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Matthew Kong
  */
+@Component(
+	property = "model.class.name=com.liferay.osb.faro.model.FaroProject",
+	service = FaroProject.class
+)
 public class FaroProjectLocalServiceImpl
 	extends FaroProjectLocalServiceBaseImpl {
 
@@ -69,7 +80,7 @@ public class FaroProjectLocalServiceImpl
 
 		long faroProjectId = counterLocalService.increment();
 
-		Group group = groupLocalService.addGroup(
+		Group group = _groupLocalService.addGroup(
 			userId, 0, FaroProject.class.getName(), faroProjectId, 0,
 			Collections.singletonMap(LocaleUtil.getDefault(), name), null,
 			GroupConstants.TYPE_SITE_PRIVATE, true,
@@ -82,7 +93,7 @@ public class FaroProjectLocalServiceImpl
 		if ((friendlyURL == null) || Validator.isBlank(friendlyURL.trim())) {
 			group.setFriendlyURL(null);
 
-			groupLocalService.updateGroup(group);
+			_groupLocalService.updateGroup(group);
 		}
 
 		long groupId = group.getGroupId();
@@ -111,9 +122,8 @@ public class FaroProjectLocalServiceImpl
 		faroProject.setTimeZoneId(timeZoneId);
 		faroProject.setWeDeployKey(weDeployKey);
 
-		faroProjectEmailAddressDomainLocalService.
-			addFaroProjectEmailAddressDomains(
-				groupId, faroProjectId, emailAddressDomains);
+		_faroProjectEmailDomainLocalService.addFaroProjectEmailDomains(
+			groupId, faroProjectId, emailAddressDomains);
 
 		return faroProjectPersistence.update(faroProject);
 	}
@@ -123,13 +133,23 @@ public class FaroProjectLocalServiceImpl
 	public FaroProject deleteFaroProjectByGroupId(long groupId)
 		throws PortalException {
 
-		faroChannelLocalService.deleteFaroChannels(groupId);
-		faroPreferencesLocalService.deleteFaroPreferencesByGroupId(groupId);
-		faroUserLocalService.deleteFaroUsers(groupId);
+		_faroChannelLocalService.deleteFaroChannels(groupId);
+		_faroPreferencesLocalService.deleteFaroPreferencesByGroupId(groupId);
+		_faroUserLocalService.deleteFaroUsers(groupId);
 
-		groupLocalService.deleteGroup(groupId);
+		_groupLocalService.deleteGroup(groupId);
 
 		return faroProjectPersistence.removeByGroupId(groupId);
+	}
+
+	@Override
+	public <T> T dslQuery(DSLQuery dslQuery) {
+		return faroProjectPersistence.dslQuery(dslQuery);
+	}
+
+	@Override
+	public int dslQueryCount(DSLQuery dslQuery) {
+		return faroProjectPersistence.dslQueryCount(dslQuery);
 	}
 
 	@Override
@@ -188,35 +208,35 @@ public class FaroProjectLocalServiceImpl
 			faroProjectLocalService.getFaroProjectsByEmailAddressDomain(
 				StringUtil.extractLast(user.getEmailAddress(), CharPool.AT));
 
-		List<Group> groups = groupLocalService.getUserGroups(
+		List<Group> groups = _groupLocalService.getUserGroups(
 			user.getUserId(), QueryUtil.ALL_POS, QueryUtil.ALL_POS,
 			new GroupNameComparator(true));
 
-		Stream<Group> groupsStream = groups.stream();
+		List<Long> groupIds = new ArrayList<>();
 
-		Set<Long> groupIds = groupsStream.map(
-			Group::getGroupId
-		).collect(
-			Collectors.toSet()
-		);
+		for (Group group : groups) {
+			groupIds.add(group.getGroupId());
+		}
 
-		Stream<FaroProject> faroProjectsStream = faroProjects.stream();
+		List<FaroProject> faroProjectsFiltered = new ArrayList<>();
 
-		return faroProjectsStream.filter(
-			faroProject ->
-				!groupIds.contains(faroProject.getGroupId()) &&
+		for (FaroProject faroProject : faroProjects) {
+			if (!groupIds.contains(faroProject.getGroupId()) &&
 				StringUtil.equals(
-					faroProject.getState(), FaroProjectConstants.STATE_READY)
-		).collect(
-			Collectors.toList()
-		);
+					faroProject.getState(), FaroProjectConstants.STATE_READY)) {
+
+				faroProjectsFiltered.add(faroProject);
+			}
+		}
+
+		return faroProjectsFiltered;
 	}
 
 	@Override
 	public void sendCreatedWorkspaceEmail(String weDeployKey) throws Exception {
 		FaroProject faroProject = fetchFaroProjectByWeDeployKey(weDeployKey);
 
-		FaroUser faroUser = faroUserLocalService.fetchOwnerFaroUser(
+		FaroUser faroUser = _faroUserLocalService.fetchOwnerFaroUser(
 			faroProject.getGroupId());
 
 		if (faroUser == null) {
@@ -227,10 +247,13 @@ public class FaroProjectLocalServiceImpl
 			getClassLoader(),
 			"com/liferay/osb/faro/dependencies/created-workspace.html");
 
-		User user = userLocalService.getUser(faroProject.getUserId());
+		User user = _userLocalService.getUser(faroProject.getUserId());
 
 		ResourceBundle resourceBundle = ResourceBundleUtil.getBundle(
 			"content.Language", user.getLocale(), getClass());
+
+		String workspaceURL = EmailUtil.getWorkspaceURL(
+			_groupLocalService.fetchGroup(faroProject.getGroupId()));
 
 		body = StringUtil.replace(
 			body,
@@ -244,7 +267,7 @@ public class FaroProjectLocalServiceImpl
 				"[$TITLE_ICON_URL$]"
 			},
 			new String[] {
-				_language.get(resourceBundle, "go-to-workspace"), _FARO_URL,
+				_language.get(resourceBundle, "go-to-workspace"), workspaceURL,
 				_language.format(
 					resourceBundle, "email-need-more-help",
 					new String[] {
@@ -253,8 +276,8 @@ public class FaroProjectLocalServiceImpl
 						"</a>"
 					}),
 				StringBundler.concat(
-					"<a class=\"body-link\" href=\"", _FARO_URL, "\">",
-					_FARO_URL, "</a>"),
+					"<a class=\"body-link\" href=\"", workspaceURL, "\">",
+					workspaceURL, "</a>"),
 				EmailUtil.getLogoIconURL(),
 				_language.get(resourceBundle, "welcome-to-analytics-cloud"),
 				_language.format(
@@ -334,10 +357,29 @@ public class FaroProjectLocalServiceImpl
 	private static final Log _log = LogFactoryUtil.getLog(
 		FaroProjectLocalServiceImpl.class);
 
-	@ServiceReference(type = Language.class)
+	@Reference
+	private FaroChannelLocalService _faroChannelLocalService;
+
+	@Reference
+	private FaroPreferencesLocalService _faroPreferencesLocalService;
+
+	@Reference
+	private FaroProjectEmailDomainLocalService
+		_faroProjectEmailDomainLocalService;
+
+	@Reference
+	private FaroUserLocalService _faroUserLocalService;
+
+	@Reference
+	private GroupLocalService _groupLocalService;
+
+	@Reference
 	private Language _language;
 
-	@ServiceReference(type = MailService.class)
+	@Reference
 	private MailService _mailService;
+
+	@Reference
+	private UserLocalService _userLocalService;
 
 }

interaminense/liferay-portal

modules/dxp/apps/osb/osb-faro/osb-faro-service/src/main/java/com/liferay/osb/faro/service/impl/FaroProjectLocalServiceImpl.java
@@ -20,10 +20,16 @@ import com.liferay.osb.faro.constants.DocumentationConstants;
 import com.liferay.osb.faro.constants.FaroProjectConstants;
 import com.liferay.osb.faro.model.FaroProject;
 import com.liferay.osb.faro.model.FaroUser;
+import com.liferay.osb.faro.service.FaroChannelLocalService;
+import com.liferay.osb.faro.service.FaroPreferencesLocalService;
+import com.liferay.osb.faro.service.FaroProjectEmailDomainLocalService;
+import com.liferay.osb.faro.service.FaroUserLocalService;
 import com.liferay.osb.faro.service.base.FaroProjectLocalServiceBaseImpl;
 import com.liferay.osb.faro.util.EmailUtil;
+import com.liferay.petra.sql.dsl.query.DSLQuery;
 import com.liferay.petra.string.CharPool;
 import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.aop.AopService;
 import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.language.Language;
@@ -34,30 +40,35 @@ import com.liferay.portal.kernel.model.GroupConstants;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.search.Indexable;
 import com.liferay.portal.kernel.search.IndexableType;
+import com.liferay.portal.kernel.service.GroupLocalService;
+import com.liferay.portal.kernel.service.UserLocalService;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.ResourceBundleUtil;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.util.comparator.GroupNameComparator;
-import com.liferay.portal.spring.extender.service.ServiceReference;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.ResourceBundle;
-import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import javax.mail.internet.InternetAddress;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Matthew Kong
  */
+@Component(
+	property = "model.class.name=com.liferay.osb.faro.model.FaroProject",
+	service = AopService.class
+)
 public class FaroProjectLocalServiceImpl
 	extends FaroProjectLocalServiceBaseImpl {
 
 	@Indexable(type = IndexableType.REINDEX)
-	@Override
 	public FaroProject addFaroProject(
 			long userId, String name, String accountKey, String accountName,
 			String corpProjectName, String corpProjectUuid,
@@ -69,7 +80,7 @@ public class FaroProjectLocalServiceImpl
 
 		long faroProjectId = counterLocalService.increment();
 
-		Group group = groupLocalService.addGroup(
+		Group group = _groupLocalService.addGroup(
 			userId, 0, FaroProject.class.getName(), faroProjectId, 0,
 			Collections.singletonMap(LocaleUtil.getDefault(), name), null,
 			GroupConstants.TYPE_SITE_PRIVATE, true,
@@ -82,7 +93,7 @@ public class FaroProjectLocalServiceImpl
 		if ((friendlyURL == null) || Validator.isBlank(friendlyURL.trim())) {
 			group.setFriendlyURL(null);
 
-			groupLocalService.updateGroup(group);
+			_groupLocalService.updateGroup(group);
 		}
 
 		long groupId = group.getGroupId();
@@ -111,76 +122,73 @@ public class FaroProjectLocalServiceImpl
 		faroProject.setTimeZoneId(timeZoneId);
 		faroProject.setWeDeployKey(weDeployKey);
 
-		faroProjectEmailAddressDomainLocalService.
-			addFaroProjectEmailAddressDomains(
-				groupId, faroProjectId, emailAddressDomains);
+		_faroProjectEmailDomainLocalService.addFaroProjectEmailDomains(
+			groupId, faroProjectId, emailAddressDomains);
 
 		return faroProjectPersistence.update(faroProject);
 	}
 
 	@Indexable(type = IndexableType.DELETE)
-	@Override
 	public FaroProject deleteFaroProjectByGroupId(long groupId)
 		throws PortalException {
 
-		faroChannelLocalService.deleteFaroChannels(groupId);
-		faroPreferencesLocalService.deleteFaroPreferencesByGroupId(groupId);
-		faroUserLocalService.deleteFaroUsers(groupId);
+		_faroChannelLocalService.deleteFaroChannels(groupId);
+		_faroPreferencesLocalService.deleteFaroPreferencesByGroupId(groupId);
+		_faroUserLocalService.deleteFaroUsers(groupId);
 
-		groupLocalService.deleteGroup(groupId);
+		_groupLocalService.deleteGroup(groupId);
 
 		return faroProjectPersistence.removeByGroupId(groupId);
 	}
 
-	@Override
+	public <T> T dslQuery(DSLQuery dslQuery) {
+		return faroProjectPersistence.dslQuery(dslQuery);
+	}
+
+	public int dslQueryCount(DSLQuery dslQuery) {
+		return faroProjectPersistence.dslQueryCount(dslQuery);
+	}
+
 	public FaroProject fetchFaroProjectByCorpProjectUuid(
 		String corpProjectUuid) {
 
 		return faroProjectPersistence.fetchByCorpProjectUuid(corpProjectUuid);
 	}
 
-	@Override
 	public FaroProject fetchFaroProjectByGroupId(long groupId) {
 		return faroProjectPersistence.fetchByGroupId(groupId);
 	}
 
-	@Override
 	public FaroProject fetchFaroProjectByWeDeployKey(String weDeployKey) {
 		return faroProjectPersistence.fetchByWeDeployKey(weDeployKey);
 	}
 
-	@Override
 	public FaroProject getFaroProjectByGroupId(long groupId)
 		throws PortalException {
 
 		return faroProjectPersistence.findByGroupId(groupId);
 	}
 
-	@Override
 	public FaroProject getFaroProjectByWeDeployKey(String weDeployKey)
 		throws PortalException {
 
 		return faroProjectPersistence.findByWeDeployKey(weDeployKey);
 	}
 
-	@Override
 	public List<FaroProject> getFaroProjects(String serverLocation) {
 		return faroProjectPersistence.findByServerLocation(serverLocation);
 	}
 
-	@Override
 	public List<FaroProject> getFaroProjectsByEmailAddressDomain(
 		String emailAddressDomains) {
 
 		return faroProjectFinder.findByEmailAddressDomain(emailAddressDomains);
 	}
 
-	@Override
 	public List<FaroProject> getFaroProjectsByUserId(long userId) {
 		return faroProjectPersistence.findByUserId(userId);
 	}
 
-	@Override
 	public List<FaroProject> getJoinableFaroProjects(User user)
 		throws PortalException {
 
@@ -188,35 +196,34 @@ public class FaroProjectLocalServiceImpl
 			faroProjectLocalService.getFaroProjectsByEmailAddressDomain(
 				StringUtil.extractLast(user.getEmailAddress(), CharPool.AT));
 
-		List<Group> groups = groupLocalService.getUserGroups(
+		List<Group> groups = _groupLocalService.getUserGroups(
 			user.getUserId(), QueryUtil.ALL_POS, QueryUtil.ALL_POS,
 			new GroupNameComparator(true));
 
-		Stream<Group> groupsStream = groups.stream();
+		List<Long> groupIds = new ArrayList<>();
 
-		Set<Long> groupIds = groupsStream.map(
-			Group::getGroupId
-		).collect(
-			Collectors.toSet()
-		);
+		for (Group group : groups) {
+			groupIds.add(group.getGroupId());
+		}
 
-		Stream<FaroProject> faroProjectsStream = faroProjects.stream();
+		List<FaroProject> faroProjectsFiltered = new ArrayList<>();
 
-		return faroProjectsStream.filter(
-			faroProject ->
-				!groupIds.contains(faroProject.getGroupId()) &&
+		for (FaroProject faroProject : faroProjects) {
+			if (!groupIds.contains(faroProject.getGroupId()) &&
 				StringUtil.equals(
-					faroProject.getState(), FaroProjectConstants.STATE_READY)
-		).collect(
-			Collectors.toList()
-		);
+					faroProject.getState(), FaroProjectConstants.STATE_READY)) {
+
+				faroProjectsFiltered.add(faroProject);
+			}
+		}
+
+		return faroProjectsFiltered;
 	}
 
-	@Override
 	public void sendCreatedWorkspaceEmail(String weDeployKey) throws Exception {
 		FaroProject faroProject = fetchFaroProjectByWeDeployKey(weDeployKey);
 
-		FaroUser faroUser = faroUserLocalService.fetchOwnerFaroUser(
+		FaroUser faroUser = _faroUserLocalService.fetchOwnerFaroUser(
 			faroProject.getGroupId());
 
 		if (faroUser == null) {
@@ -227,10 +234,13 @@ public class FaroProjectLocalServiceImpl
 			getClassLoader(),
 			"com/liferay/osb/faro/dependencies/created-workspace.html");
 
-		User user = userLocalService.getUser(faroProject.getUserId());
+		User user = _userLocalService.getUser(faroProject.getUserId());
 
 		ResourceBundle resourceBundle = ResourceBundleUtil.getBundle(
 			"content.Language", user.getLocale(), getClass());
+
+		String workspaceURL = EmailUtil.getWorkspaceURL(
+			_groupLocalService.fetchGroup(faroProject.getGroupId()));
 
 		body = StringUtil.replace(
 			body,
@@ -244,7 +254,7 @@ public class FaroProjectLocalServiceImpl
 				"[$TITLE_ICON_URL$]"
 			},
 			new String[] {
-				_language.get(resourceBundle, "go-to-workspace"), _FARO_URL,
+				_language.get(resourceBundle, "go-to-workspace"), workspaceURL,
 				_language.format(
 					resourceBundle, "email-need-more-help",
 					new String[] {
@@ -253,8 +263,8 @@ public class FaroProjectLocalServiceImpl
 						"</a>"
 					}),
 				StringBundler.concat(
-					"<a class=\"body-link\" href=\"", _FARO_URL, "\">",
-					_FARO_URL, "</a>"),
+					"<a class=\"body-link\" href=\"", workspaceURL, "\">",
+					workspaceURL, "</a>"),
 				EmailUtil.getLogoIconURL(),
 				_language.get(resourceBundle, "welcome-to-analytics-cloud"),
 				_language.format(
@@ -297,7 +307,6 @@ public class FaroProjectLocalServiceImpl
 	}
 
 	@Indexable(type = IndexableType.REINDEX)
-	@Override
 	public FaroProject updateState(long faroProjectId, String state) {
 		FaroProject faroProject = faroProjectPersistence.fetchByPrimaryKey(
 			faroProjectId);
@@ -312,7 +321,6 @@ public class FaroProjectLocalServiceImpl
 	}
 
 	@Indexable(type = IndexableType.REINDEX)
-	@Override
 	public FaroProject updateSubscription(
 		long faroProjectId, String subscription) {
 
@@ -334,10 +342,29 @@ public class FaroProjectLocalServiceImpl
 	private static final Log _log = LogFactoryUtil.getLog(
 		FaroProjectLocalServiceImpl.class);
 
-	@ServiceReference(type = Language.class)
+	@Reference
+	private FaroChannelLocalService _faroChannelLocalService;
+
+	@Reference
+	private FaroPreferencesLocalService _faroPreferencesLocalService;
+
+	@Reference
+	private FaroProjectEmailDomainLocalService
+		_faroProjectEmailDomainLocalService;
+
+	@Reference
+	private FaroUserLocalService _faroUserLocalService;
+
+	@Reference
+	private GroupLocalService _groupLocalService;
+
+	@Reference
 	private Language _language;
 
-	@ServiceReference(type = MailService.class)
+	@Reference
 	private MailService _mailService;
+
+	@Reference
+	private UserLocalService _userLocalService;
 
 }