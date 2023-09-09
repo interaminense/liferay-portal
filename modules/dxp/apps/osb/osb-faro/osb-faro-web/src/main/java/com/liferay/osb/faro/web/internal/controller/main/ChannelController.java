9f7eab82bf8531459676c9899110439f3b1bb213
Leslie Wong <8095751+lesliewong92@users.noreply.github.com>
2023-09-08 17:19:46 -0700

    LRAC-14489 Create request from Aash

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/java/com/liferay/osb/faro/web/internal/controller/main/ChannelController.java
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/java/com/liferay/osb/faro/web/internal/controller/main/ChannelController.java
@@ -38,6 +38,7 @@ import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.OrderByComparator;
 import com.liferay.portal.kernel.util.StringUtil;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -295,8 +296,20 @@ public class ChannelController extends BaseFaroController {
 			groupId, query, startAndEnd[0], startAndEnd[1],
 			new FaroChannelComparator(orderByFields));
 
+		List<FaroChannelDisplay> faroChannelDisplays = new ArrayList<>();
+
+		for (FaroChannel faroChannel : faroChannels) {
+			faroChannelDisplays.add(
+				new FaroChannelDisplay(
+					contactsEngineClient.getChannel(
+						faroProjectLocalService.getFaroProjectByGroupId(
+							groupId),
+						faroChannel.getChannelId()),
+					faroChannel));
+		}
+
 		return new FaroResultsDisplay<>(
-			TransformUtil.transform(faroChannels, FaroChannelDisplay::new),
+			faroChannelDisplays,
 			_faroChannelLocalService.searchCount(groupId, query));
 	}
 
