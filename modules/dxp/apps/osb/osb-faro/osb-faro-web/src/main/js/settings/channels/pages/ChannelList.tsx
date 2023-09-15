969263f1ab3f8c43872b8b93acb136d213c4ed26
Adriano Interaminense <adriano.interaminense@gmail.com>
2023-09-15 13:55:16 -0300

    LRAC-14493 Rename fn names

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/settings/channels/pages/ChannelList.tsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/settings/channels/pages/ChannelList.tsx
@@ -115,8 +115,8 @@ const ChannelList: React.FC<IChannelListProps> = ({
 
 	const handleAddChannel = () => {
 		open(modalTypes.ADD_CHANNEL_MODAL, {
-			onCloseFn: close,
-			onSubmitFn: handleSubmit
+			onClose: close,
+			onSubmit: handleSubmit
 		});
 	};
 
