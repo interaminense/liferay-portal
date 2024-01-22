ae8323088dfad475b5240d71f7d4991ee360b392
Marcio Fonseca <marcio.fonseca@liferay.com>
2024-01-22 13:09:26 -0300

    LPD-6936 create and update unit tests

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/sites/touchpoints/components/sankey/__tests__/PagePathCard.tsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/sites/touchpoints/components/sankey/__tests__/PagePathCard.tsx
@@ -2,12 +2,12 @@ import client from 'shared/apollo/client';
 import PagePathCard from '../PagePathCard';
 import React from 'react';
 import {ApolloProvider} from '@apollo/react-components';
+import {BrowserRouter} from 'react-router-dom';
 import {CHART_COLORS, MAIN_NODE_COLOR, SECONDARY_NODE_COLOR} from '../utils';
-import {cleanup, fireEvent, render} from '@testing-library/react';
+import {cleanup, fireEvent, getByTestId, render} from '@testing-library/react';
 import {MockedProvider} from '@apollo/react-testing';
 import {mockPagePathReq} from 'test/graphql-data';
 import {RangeKeyTimeRanges} from 'shared/util/constants';
-import {StaticRouter} from 'react-router';
 import {waitForLoadingToBeRemoved} from 'test/helpers';
 
 jest.unmock('react-dom');
@@ -16,6 +16,7 @@ jest.mock('react-router-dom', () => ({
 	...jest.requireActual('react-router-dom'),
 	useParams: () => ({
 		channelId: '123',
+		groupId: '4567',
 		rangeKey: '30',
 		title: 'Liferay DXP - Home',
 		touchpoint: 'https://liferay.com/home'
@@ -26,30 +27,35 @@ const PREVIOUS_PATH_NODES = [
 	{
 		__typename: 'PagePathNode',
 		canonicalUrl: 'https://www.site1.com',
+		external: false,
 		title: 'Site 1',
 		views: 10000
 	},
 	{
 		__typename: 'PagePathNode',
 		canonicalUrl: 'https://www.site2.com',
+		external: false,
 		title: 'Site 2',
 		views: 10000
 	},
 	{
 		__typename: 'PagePathNode',
 		canonicalUrl: 'https://www.site3.com',
+		external: false,
 		title: 'Site 3',
 		views: 5000
 	},
 	{
 		__typename: 'PagePathNode',
 		canonicalUrl: 'others',
+		external: false,
 		title: 'others',
 		views: 500
 	},
 	{
 		__typename: 'PagePathNode',
 		canonicalUrl: 'drop-offs',
+		external: false,
 		title: 'drop-offs',
 		views: 8000
 	}
@@ -59,30 +65,35 @@ const FOLLOWING_PATH_NODES = [
 	{
 		__typename: 'PagePathNode',
 		canonicalUrl: 'https://www.google.com',
+		external: true,
 		title: 'Google',
 		views: 10000
 	},
 	{
 		__typename: 'PagePathNode',
 		canonicalUrl: 'https://www.facebook.com',
+		external: true,
 		title: 'Facebook',
 		views: 10000
 	},
 	{
 		__typename: 'PagePathNode',
 		canonicalUrl: 'https://www.instagram.com',
+		external: true,
 		title: 'Instagram',
 		views: 8000
 	},
 	{
 		__typename: 'PagePathNode',
 		canonicalUrl: 'direct',
+		external: false,
 		title: 'direct',
 		views: 5000
 	},
 	{
 		__typename: 'PagePathNode',
 		canonicalUrl: 'others',
+		external: false,
 		title: 'others',
 		views: 1000
 	}
@@ -112,7 +123,7 @@ const EMPTY_STATE_DATA = {
 
 const WrapperComponent = ({data}) => (
 	<ApolloProvider client={client}>
-		<StaticRouter>
+		<BrowserRouter>
 			<MockedProvider mocks={[mockPagePathReq(data)]}>
 				<PagePathCard
 					rangeSelectors={{
@@ -122,7 +133,7 @@ const WrapperComponent = ({data}) => (
 					}}
 				/>
 			</MockedProvider>
-		</StaticRouter>
+		</BrowserRouter>
 	</ApolloProvider>
 );
 
@@ -186,7 +197,8 @@ describe('PagePathCard', () => {
 				return;
 			}
 
-			const title = node.querySelector('text:nth-of-type(2)').textContent;
+			const title = getByTestId(node as HTMLElement, 'sankey-node-title')
+				.textContent;
 
 			if (
 				title === 'Drop Offs' ||
@@ -225,7 +237,7 @@ describe('PagePathCard', () => {
 
 		expect(nodes).toHaveLength(11);
 
-		const url = getByText('https://www.site3.com');
+		const url = getByText('https://www.site3....');
 
 		fireEvent.mouseOver(url);
 
@@ -234,5 +246,29 @@ describe('PagePathCard', () => {
 		expect(getByText('Page Views | Exit Pages')).toBeInTheDocument();
 
 		expect(getByText('5,000')).toBeInTheDocument();
+	});
+
+	it('should check if tooltip are rendered', async () => {
+		const {container} = render(<WrapperComponent data={DATA} />);
+
+		await waitForLoadingToBeRemoved(container);
+
+		const nodes = container.querySelectorAll(
+			'.recharts-sankey-nodes > .recharts-layer'
+		);
+
+		const tooltip = container.querySelector('[data-tooltip-align="right"]');
+
+		expect(tooltip).toBeInTheDocument();
+
+		expect(tooltip).toHaveAttribute('title', 'Go to Dashboard Page');
+
+		/**
+		 * 5 previous node +
+		 * 5 following node +
+		 * 1 main node
+		 */
+
+		expect(nodes).toHaveLength(11);
 	});
 });
