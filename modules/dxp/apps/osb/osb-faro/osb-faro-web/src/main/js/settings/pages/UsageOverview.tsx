606a875ced915886aa27c26cd9f8463b6f1b700b
Adriano Interaminense <adriano.interaminense@gmail.com>
2024-02-15 13:32:00 -0300

    LPD-15137 Adapt components to use useCurrentUser and useTimeZone hooks

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/settings/pages/UsageOverview.tsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/settings/pages/UsageOverview.tsx
@@ -5,7 +5,7 @@ import ClayLayout from '@clayui/layout';
 import moment from 'moment';
 import React, {useState} from 'react';
 import {AlertTypes} from 'shared/components/Alert';
-import {compose, withCurrentUser, withProject} from 'shared/hoc';
+import {compose, withProject} from 'shared/hoc';
 import {CUSTOM_DATE_FORMAT, formatDateToTimeZone} from 'shared/util/date';
 import {
 	formatPlanData,
@@ -19,6 +19,8 @@ import {sub} from 'shared/util/lang';
 import {SubscriptionDetails} from 'settings/components/usage-overview/SubscriptionDetails';
 import {SubscriptionStatuses} from 'shared/util/constants';
 import {Text} from '@clayui/core';
+import {useCurrentUser} from 'shared/hooks/useCurrentUser';
+import {useTimeZone} from 'shared/hooks/useTimeZone';
 
 const subscriptionStatuses = admin => ({
 	[SubscriptionStatuses.Approaching]: {
@@ -63,12 +65,13 @@ const getAlertStatusCode = currentPlan => {
 	return null;
 };
 
-export const UsageOverview = ({currentUser, groupId, project}) => {
+export const UsageOverview = ({groupId, project}) => {
 	const [showAlert, setShowAlert] = useState(true);
+	const currentUser = useCurrentUser();
 
 	const admin = currentUser.isAdmin();
 	const currentPlan = formatPlanData(project.faroSubscription);
-	const timeZoneId = project.timeZone.get('timeZoneId');
+	const {timeZoneId} = useTimeZone();
 	const planType =
 		PLAN_TYPES[currentPlan.name] || PLAN_TYPES[PLANS.basic.name];
 
@@ -191,4 +194,4 @@ export const UsageOverview = ({currentUser, groupId, project}) => {
 	);
 };
 
-export default compose(withCurrentUser, withProject)(UsageOverview);
+export default compose(withProject)(UsageOverview);
