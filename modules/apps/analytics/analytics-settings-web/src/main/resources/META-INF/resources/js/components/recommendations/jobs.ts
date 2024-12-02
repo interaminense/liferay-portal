6b4dfda189b3a3c164df1c24c4b2a4e3bbb4205d
Adriano Interaminense <adriano.interaminense@gmail.com>
2024-12-02 10:44:13 -0300

    LPD-20640 | LPD-24953 Integrate job action with the backend

--- modules/apps/analytics/analytics-settings-web/src/main/resources/META-INF/resources/js/components/recommendations/jobs.ts
+++ modules/apps/analytics/analytics-settings-web/src/main/resources/META-INF/resources/js/components/recommendations/jobs.ts
@@ -3,33 +3,38 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
+import {JobId} from '../../utils/api';
+
 export type Job = {
 	active: boolean;
 	description: string;
-	id: string;
+	id: JobId;
+	modalTitle: string;
 	title: string;
 	type: string;
 };
 
 export const jobs: Job[] = [
 	{
-		title: Liferay.Language.get('most-popular-content'),
+		active: false,
 		description: Liferay.Language.get(
 			'recommends-content-based-on-popularity-among-all-users-without-considering-individual-user-behavior'
 		),
-		id: '1',
+		id: JobId.ContentRecommenderMostPopularItemsEnabled,
+		modalTitle: Liferay.Language.get('most-popular-content'),
+		title: Liferay.Language.get('most-popular-content'),
 		type: Liferay.Language.get('content'),
-		active: false,
 	},
 	{
-		title: Liferay.Language.get(
-			"user's-personalized-content-recommendations"
-		),
+		active: false,
 		description: Liferay.Language.get(
 			'recommends-content-based-on-individual-users-preferences-and-past-behavior'
 		),
-		id: '2',
+		id: JobId.ContentRecommenderUserPersonalizationEnabled,
+		modalTitle: Liferay.Language.get("user's-personalized-content"),
+		title: Liferay.Language.get(
+			"user's-personalized-content-recommendations"
+		),
 		type: Liferay.Language.get('content'),
-		active: false,
 	},
 ];
