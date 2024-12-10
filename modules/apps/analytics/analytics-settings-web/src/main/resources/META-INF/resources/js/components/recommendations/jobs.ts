ecdf3098eb57185d8b828f39ef6f6de447b4dce9
Adriano Interaminense <adriano.interaminense@gmail.com>
2024-12-10 10:59:48 -0300

    LPD-42005 | LPD-41962 Refetch jobs every 3 seconds to check if all jobs were configured

--- modules/apps/analytics/analytics-settings-web/src/main/resources/META-INF/resources/js/components/recommendations/jobs.ts
+++ modules/apps/analytics/analytics-settings-web/src/main/resources/META-INF/resources/js/components/recommendations/jobs.ts
@@ -3,35 +3,38 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import {JobId} from '../../utils/api';
+import {JobId, JobStatus} from '../../utils/api';
 
 export type Job = {
-	active: boolean;
 	description: string;
+	enabled: boolean;
 	id: JobId;
 	modalTitle: string;
+	status: JobStatus;
 	title: string;
 	type: string;
 };
 
 export const jobs: Job[] = [
 	{
-		active: false,
 		description: Liferay.Language.get(
 			'recommends-content-based-on-popularity-among-all-users-without-considering-individual-user-behavior'
 		),
-		id: JobId.ContentRecommenderMostPopularItemsEnabled,
+		enabled: false,
+		id: JobId.ContentRecommenderMostPopularItems,
 		modalTitle: Liferay.Language.get('most-popular-content'),
+		status: JobStatus.Disabled,
 		title: Liferay.Language.get('most-popular-content'),
 		type: Liferay.Language.get('content'),
 	},
 	{
-		active: false,
 		description: Liferay.Language.get(
 			'recommends-content-based-on-individual-users-preferences-and-past-behavior'
 		),
-		id: JobId.ContentRecommenderUserPersonalizationEnabled,
+		enabled: false,
+		id: JobId.ContentRecommenderUserPersonalization,
 		modalTitle: Liferay.Language.get("user's-personalized-content"),
+		status: JobStatus.Disabled,
 		title: Liferay.Language.get(
 			"user's-personalized-content-recommendations"
 		),
