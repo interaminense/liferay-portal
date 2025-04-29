9cbfe090080e160fde39a00d3739cb3df5470b84
Adriano Interaminense <adriano.interaminense@gmail.com>
2025-04-29 18:51:22 -0300

    LPD-53328 Add dashboard pure components

---
+++ modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/main/dashboard/components/BaseCard.tsx
@@ -0,0 +1,46 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import {Text} from '@clayui/core';
+import React from 'react';
+
+interface IBaseCard extends React.HTMLAttributes<HTMLElement> {
+	Preferences?: React.ReactNode;
+	description?: string;
+	title: string;
+}
+
+const BaseCard: React.FC<IBaseCard> = ({
+	Preferences,
+	children,
+	description,
+	title,
+}) => {
+	return (
+		<div className="cms-dashboard__base-card p-3 rounded-lg sheet">
+			<div className="cms-dashboard__base-card__header d-flex mb-3">
+				<div className="flex-grow-1">
+					<Text size={4} weight="semi-bold">
+						{title.toUpperCase()}
+					</Text>
+
+					{description && (
+						<div>
+							<Text color="secondary" size={3}>
+								{description}
+							</Text>
+						</div>
+					)}
+				</div>
+
+				{Preferences}
+			</div>
+
+			{children}
+		</div>
+	);
+};
+
+export {BaseCard};
