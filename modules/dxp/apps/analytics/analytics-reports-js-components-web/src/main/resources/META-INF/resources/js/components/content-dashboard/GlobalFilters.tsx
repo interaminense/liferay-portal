295ebed12a244e2ffaf0c77eddd7e4b2a5e757bc
Adriano Interaminense <adriano.interaminense@gmail.com>
2025-08-06 16:35:31 -0300

    LPD-62036 Separate the mixed logic of the content-dashboad components in GlobalFilters

--- modules/dxp/apps/analytics/analytics-reports-js-components-web/src/main/resources/META-INF/resources/js/components/content-dashboard/GlobalFilters.tsx
+++ modules/dxp/apps/analytics/analytics-reports-js-components-web/src/main/resources/META-INF/resources/js/components/content-dashboard/GlobalFilters.tsx
@@ -3,79 +3,22 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import React, {useContext} from 'react';
+import React from 'react';
 
-import {Context} from '../../Context';
-import {Individuals} from '../../types/global';
-import Filter from '../Filter';
-import {
-	RangeSelectors,
-	RangeSelectorsDropdown,
-} from '../RangeSelectorsDropdown';
 import Title from '../Title';
+import {AllIndividualsFilter} from './AllIndividualsFilter';
+import {RangeSelectorsFilter} from './RangeSelectorsFilter';
 
-const individualFilterLang = {
-	[Individuals.AllIndividuals]: Liferay.Language.get('all-individuals'),
-	[Individuals.AnonymousIndividuals]: Liferay.Language.get(
-		'anonymous-individuals'
-	),
-	[Individuals.KnownIndividuals]: Liferay.Language.get('known-individuals'),
-};
+const GlobalFilters = () => (
+	<div className="d-flex global-filters justify-content-between mb-3">
+		<Title value={Liferay.Language.get('overview')} />
 
-const GlobalFilters = () => {
-	const {changeIndividualFilter, changeRangeSelectorFilter, filters} =
-		useContext(Context);
+		<div className="d-flex">
+			<AllIndividualsFilter />
 
-	const rangeSelectors = [
-		RangeSelectors.Last7Days,
-		RangeSelectors.Last28Days,
-		RangeSelectors.Last30Days,
-		RangeSelectors.Last90Days,
-	];
-
-	if (process.env.NODE_ENV === 'development') {
-		rangeSelectors.push(RangeSelectors.Last24Hours);
-	}
-
-	return (
-		<div className="d-flex global-filters justify-content-between mb-3">
-			<Title value={Liferay.Language.get('overview')} />
-
-			<div className="d-flex">
-				<Filter
-					active={filters.individual}
-					className="mr-3"
-					filterByValue="individuals"
-					icon="users"
-					items={[
-						{
-							label: Liferay.Language.get('all-individuals'),
-							value: Individuals.AllIndividuals,
-						},
-						{
-							label: Liferay.Language.get(
-								'anonymous-individuals'
-							),
-							value: Individuals.AnonymousIndividuals,
-						},
-						{
-							label: Liferay.Language.get('known-individuals'),
-							value: Individuals.KnownIndividuals,
-						},
-					]}
-					onSelectItem={(item) => changeIndividualFilter(item.value)}
-					triggerLabel={individualFilterLang[filters.individual]}
-				/>
-
-				<RangeSelectorsDropdown
-					activeRangeSelector={filters.rangeSelector}
-					availableRangeKeys={rangeSelectors}
-					onChange={changeRangeSelectorFilter}
-					size="xs"
-				/>
-			</div>
+			<RangeSelectorsFilter />
 		</div>
-	);
-};
+	</div>
+);
 
 export default GlobalFilters;
