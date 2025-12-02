f663a0f5fbed648d43a02fc6d67951b17e098e90
Adriano Interaminense <adriano.interaminense@gmail.com>
2025-12-02 14:13:21 -0300

    LPD-60291 Create new Liferay DXP data source connection screen and setup 'copy token' and 'review synced data' steps

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/settings/pages/data-source/ConnectLiferayDXP.tsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/settings/pages/data-source/ConnectLiferayDXP.tsx
@@ -1,5 +1,25 @@
 import React from 'react';
+import WizardPage, {Step} from 'settings/components/base-page/WizardPage';
+import {ConnectLiferayDXPStep} from 'settings/components/liferay/steps/ConnectLiferayDXPStep';
+import {ReviewSyncedDataStep} from 'settings/components/liferay/steps/ReviewSyncedDataStep';
 
-const ConnectLiferayDXP = () => <div>hello, world!</div>;
+const steps: Step[] = [
+	{
+		content: props => <ConnectLiferayDXPStep {...props} />,
+		description: Liferay.Language.get(
+			'connect-your-dxp-instance-to-analytics-cloud-to-start-tracking-users-visits-and-interactions-within-your-sites'
+		),
+		title: Liferay.Language.get('connect-your-dxp-analytics')
+	},
+	{
+		content: props => <ReviewSyncedDataStep {...props} />,
+		description: Liferay.Language.get(
+			'while-your-data-continues-syncing-in-the-background,-you-may-proceed-with-the-setup-process.-to-monitor-the-sync-status-at-any-time,-go-to-settings-in-data-sources'
+		),
+		title: Liferay.Language.get('review-synced-data')
+	}
+];
 
-export default ConnectLiferayDXP;
+const ConnectSalesforce = () => <WizardPage steps={steps} />;
+
+export default ConnectSalesforce;
