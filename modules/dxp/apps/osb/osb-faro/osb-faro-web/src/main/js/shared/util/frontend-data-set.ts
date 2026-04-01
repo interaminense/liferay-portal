35a7d03d823e066c766df341aa2934563209981f
Adriano Interaminense <adriano.interaminense@gmail.com>
2026-04-01 16:29:34 -0300

    LPD-82436 Add sidebar with tags and categories

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/shared/util/frontend-data-set.ts
+++
@@ -1,9 +0,0 @@
-import FaroConstants from 'shared/util/constants';
-
-const {cur, delta, deltaValues} = FaroConstants.pagination;
-
-export const pagination = {
-	deltas: deltaValues.map(delta => ({label: delta})),
-	initialDelta: delta,
-	initialPageNumber: cur
-};
