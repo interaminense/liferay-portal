6b9920872e3fed630a0eabdbd89991db606fb1f0
Adriano Interaminense <adriano.interaminense@gmail.com>
2026-04-04 21:37:22 -0300

    LPD-0 Create Liferay Data Platform Site Initializer

---
+++ modules/apps/site/site-ldp-site-initializer/src/main/resources/META-INF/resources/js/components/App.tsx
@@ -0,0 +1,20 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import React from 'react';
+
+interface Props {
+	portletNamespace?: string;
+}
+
+const App: React.FC<Props> = ({portletNamespace}) => {
+	return (
+		<div className="liferay-data-platform-app" id={`${portletNamespace}root`}>
+			Hello, Liferay Data Platform!
+		</div>
+	);
+};
+
+export default App;
