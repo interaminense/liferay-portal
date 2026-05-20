233dc468eebc5ebeef7d81bc5673dbf6c5f005b8
Adriano Interaminense <adriano.interaminense@liferay.com>
2026-05-20 13:17:26 -0300

    LPD-89684 proxy webpack dev server to remote Liferay envs

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/webpack.dev.js
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/webpack.dev.js
@@ -1,5 +1,5 @@
 const common = require('./webpack.common');
-const {responseInterceptor} = require('http-proxy-middleware');
+const {createOnProxyRes} = require('./webpack.dev.proxy');
 const {merge} = require('webpack-merge');
 const webpack = require('webpack');
 
@@ -9,45 +9,6 @@ const TARGET = (process.env.FARO_URL || 'http://0.0.0.0:8080').replace(
 	/\/$/,
 	''
 );
-
-// The dev server proxies to a remote Liferay (e.g. analytics-stg) whose
-// `web.server.host` makes it emit absolute URLs back to itself. We buffer
-// each response and rewrite those URLs (in Location, Set-Cookie Domain, and
-// the body) so the browser stays on the dev server instead of leaving for
-// the upstream host.
-
-async function rewriteUpstreamResponse(responseBuffer, proxyRes, req, res) {
-	const proxyOrigin = `http://${req.headers.host}`;
-
-	if (proxyRes.headers['location']) {
-		res.setHeader(
-			'location',
-			proxyRes.headers['location'].split(TARGET).join(proxyOrigin)
-		);
-	}
-
-	if (proxyRes.headers['set-cookie']) {
-		res.setHeader(
-			'set-cookie',
-			proxyRes.headers['set-cookie'].map(cookie =>
-				cookie.replace(/;\s*Domain=[^;]+/gi, '')
-			)
-		);
-	}
-
-	const contentType = proxyRes.headers['content-type'] || '';
-	const isTextual =
-		contentType.includes('text/html') ||
-		contentType.includes('javascript') ||
-		contentType.includes('application/json') ||
-		contentType.includes('text/css');
-
-	if (!isTextual) {
-		return responseBuffer;
-	}
-
-	return responseBuffer.toString('utf8').split(TARGET).join(proxyOrigin);
-}
 
 module.exports = merge(common.config, {
 	devServer: {
@@ -59,7 +20,7 @@ module.exports = merge(common.config, {
 		proxy: {
 			'**': {
 				changeOrigin: true,
-				onProxyRes: responseInterceptor(rewriteUpstreamResponse),
+				onProxyRes: createOnProxyRes(TARGET),
 				selfHandleResponse: true,
 				target: TARGET
 			}
