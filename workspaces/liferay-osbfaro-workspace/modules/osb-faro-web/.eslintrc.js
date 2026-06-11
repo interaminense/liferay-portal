9a06a6b27c4e407e041e94fe206acbf9998de336
Adriano Interaminense <adriano.interaminense@liferay.com>
2026-06-11 12:01:10 -0300

    LPD-93151 Configure the frontend source formatter for osb-faro-web

--- workspaces/liferay-osbfaro-workspace/modules/osb-faro-web/.eslintrc.js
+++ workspaces/liferay-osbfaro-workspace/modules/osb-faro-web/.eslintrc.js
@@ -3,150 +3,76 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-module.exports = {
+const path = require('path');
+
+const config = {
 	env: {
 		browser: true,
-		es6: true,
-		jest: true,
-		node: true
+		es2021: true,
+		node: true,
 	},
-	extends: [
-		'liferay',
-		'plugin:jsx-a11y/recommended',
-		'plugin:prettier/recommended'
-	],
+	extends: ['plugin:@liferay/portal'],
 	globals: {
-		analytics: true,
 		FARO_DEV_MODE: true,
 		FARO_ENV: true,
 		FARO_PENDO_API_KEY: true,
 		IncrementalDOM: true,
 		Liferay: true,
-		pendo: true
+		analytics: true,
+		configuration: true,
+		fragmentElement: true,
+		fragmentNamespace: true,
+		layoutMode: true,
+		pendo: true,
+		themeDisplay: true,
 	},
-	parser: '@typescript-eslint/parser',
+	ignorePatterns: ['!*'],
+	overrides: [
+		{
+			env: {
+				jest: true,
+				node: true,
+			},
+			files: [
+				'**/__mocks__/**/*.{js,jsx,ts,tsx}',
+				'**/__tests__/**/*.{js,jsx,ts,tsx}',
+				'**/test/**/*.{js,jsx,ts,tsx}',
+			],
+		},
+	],
 	parserOptions: {
 		ecmaFeatures: {
-			allowImportExportEverywhere: true,
-			experimentalObjectRestSpread: true,
 			jsx: true,
-			legacyDecorators: true
+			legacyDecorators: true,
 		},
-		ecmaVersion: 2020,
-		sourceType: 'module'
+		ecmaVersion: 2023,
 	},
-	plugins: [
-		'react',
-		'sort-destructure-keys',
-		'sort-imports-es6-autofix',
-		'jsx-a11y',
-		'@typescript-eslint'
-	],
+	plugins: ['@liferay'],
 	root: true,
 	rules: {
-		'@typescript-eslint/no-unused-vars': [
+		'@liferay/empty-line-between-elements': 'off',
+		'@liferay/import-extensions': 'off',
+		'@liferay/portal/deprecation': 'off',
+		'@liferay/portal/no-document-cookie': 'off',
+		'@liferay/portal/no-explicit-extend': 'off',
+		'@liferay/portal/no-global-fetch': 'off',
+		'@liferay/portal/no-global-storage': 'off',
+		'@liferay/portal/no-loader-import-specifier': 'off',
+		'@liferay/portal/no-localhost-reference': 'off',
+		'@liferay/portal/no-react-dom-create-portal': 'off',
+		'@liferay/portal/no-react-dom-render': 'off',
+		'@liferay/portal/no-side-navigation': 'off',
+		'@liferay/portal/unexecuted-ismounted': 'off',
+		'no-empty': ['error', {allowEmptyCatch: true}],
+		'notice/notice': [
 			'error',
 			{
-				args: 'after-used',
-				ignoreRestSiblings: false,
-				vars: 'all'
-			}
+				nonMatchingTolerance: 0.7,
+				onNonMatchingHeader: 'replace',
+				templateFile: path.join(__dirname, 'copyright.js'),
+			},
 		],
-		'arrow-body-style': [2, 'as-needed'],
-		'comma-dangle': 0,
-		'default-case': 2,
-		'jsx-a11y/label-has-associated-control': 2,
-		'jsx-a11y/label-has-for': 0,
-		'jsx-a11y/no-autofocus': [
-			2,
-			{
-				ignoreNonDOM: true
-			}
-		],
-		'max-len': 0,
-		'new-cap': [
-			2,
-			{
-				capIsNew: false
-			}
-		],
-		'no-console': 2,
-		'no-import-assign': 0,
-		'no-mixed-spaces-and-tabs': [2, 'smart-tabs'],
-		'no-return-assign': [2, 'always'],
-		'no-undef': 2,
-		'no-unused-vars': 0,
-		'object-shorthand': 2,
-		'prefer-const': 2,
-		'prefer-template': 2,
-		'quote-props': [2, 'as-needed'],
-		quotes: [2, 'single', 'avoid-escape'],
-		'react/jsx-boolean-value': 2,
-		'react/jsx-curly-brace-presence': [
-			2,
-			{
-				children: 'ignore',
-				props: 'never'
-			}
-		],
-		'react/jsx-fragments': [2, 'syntax'],
-		'react/jsx-handler-names': 2,
-		'react/jsx-key': 2,
-		'react/jsx-no-literals': 2,
-		'react/jsx-sort-props': [
-			2,
-			{
-				ignoreCase: true
-			}
-		],
-		'react/jsx-uses-react': 2,
-		'react/jsx-uses-vars': 2,
-		'react/sort-comp': [
-			2,
-			{
-				groups: {
-					rendering: ['/^render.+$/', 'render']
-				},
-				order: [
-					'static-variables',
-					'contextType',
-					'contextTypes',
-					'childContextType',
-					'defaultProps',
-					'propTypes',
-					'state',
-					'static-methods',
-					'instance-variables',
-					'constructor',
-					'lifecycle',
-					'everything-else',
-					'render'
-				]
-			}
-		],
-		'require-jsdoc': 0,
-		'sort-destructure-keys/sort-destructure-keys': 2,
-		'sort-imports-es6-autofix/sort-imports-es6': [
-			2,
-			{
-				ignoreCase: true,
-				ignoreMemberSort: false,
-				memberSyntaxSortOrder: ['none', 'all', 'single', 'multiple']
-			}
-		],
-		'sort-keys': [
-			2,
-			'asc',
-			{
-				caseSensitive: false
-			}
-		],
-		'sort-vars': 2,
-		'valid-jsdoc': 0
 	},
-	settings: {
-		react: {
-			version: 'detect'
-		}
-	}
 };
+
+module.exports = config;
