77b3eee65d07079598715f5ad222361aa58e4499
Adriano Interaminense <adriano.interaminense@liferay.com>
2026-06-11 16:35:30 -0300

    LPD-94265 SF

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/individual/profile/pages/OverviewCDP.jsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/individual/profile/pages/OverviewCDP.jsx
@@ -3,27 +3,25 @@ import ProfileCardCDP from '../hoc/ProfileCardCDP';
 import React from 'react';
 import {connect} from 'react-redux';
 
-const Overview = ({channelId, groupId, individual, tabId, timeZoneId}) => {
-	return (
-		<div className='overview-column-main'>
-			<ContextualInformation
-				contactId={individual.get('id')}
-				contextData={individual.get('context')}
-				email={individual.getIn(['properties', 'email'])}
-				userId={individual.getIn(['properties', 'userId'])}
-				uuid={individual.getIn(['properties', 'uuid'])}
-			/>
+const Overview = ({channelId, groupId, individual, tabId, timeZoneId}) => (
+	<div className='overview-column-main'>
+		<ContextualInformation
+			contactId={individual.get('id')}
+			contextData={individual.get('context')}
+			email={individual.getIn(['properties', 'email'])}
+			userId={individual.getIn(['properties', 'userId'])}
+			uuid={individual.getIn(['properties', 'uuid'])}
+		/>
 
-			<ProfileCardCDP
-				channelId={channelId}
-				entity={individual}
-				groupId={groupId}
-				tabId={tabId}
-				timeZoneId={timeZoneId}
-			/>
-		</div>
-	);
-};
+		<ProfileCardCDP
+			channelId={channelId}
+			entity={individual}
+			groupId={groupId}
+			tabId={tabId}
+			timeZoneId={timeZoneId}
+		/>
+	</div>
+);
 
 export default connect((store, {groupId}) => ({
 	timeZoneId: store.getIn([
